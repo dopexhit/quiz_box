797466b8c5c139cfef40788a0571f52f163941cb
dopexhit <mayankbhola4@gmail.com>
2020-10-16 19:19:37 +0530

    splash screen added

---
+++ lib/screens/login_signup.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class LoginSignup extends StatefulWidget {
+  @override
+  _LoginSignupState createState() => _LoginSignupState();
+}
+
+class _LoginSignupState extends State<LoginSignup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
