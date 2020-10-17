c1787120923c0fd47c84d9faea3b417253266130
Piyush Nirala <nirala.raghav7@gmail.com>
2020-10-17 15:54:28 +0530

    login background color and MyApp() added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:quiz_box/screens/splash.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: SplashPage(),
-  ));
+import 'screens/splash.dart';
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Login',
+      //darkTheme: ThemeData.dark(),
+      home: SplashPage(),
+    );
+  }
+}
--- lib/screens/login_signup.dart
+++ lib/screens/login_signup.dart
@@ -10,7 +10,7 @@ class _LoginSignupState extends State<LoginSignup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
           Container(
